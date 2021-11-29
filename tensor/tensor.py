8aaaca021d84aceaf02c4c8adf80b71049df25ca
Vadim Pechenin <vadim.pechenin2015@gmail.com>
2021-11-29 10:41:25 +0400

    Add Support for Additional Functions

--- tensor/tensor.py
+++ tensor/tensor.py
@@ -71,6 +71,39 @@ class Tensor (object):
                     self.creators[0].backward(self.grad, self)
                     self.creators[1].backward(self.grad, self)
 
+                if (self.creation_op == "sub"):
+                    self.creators[0].backward(Tensor(self.grad.data), self)
+                    self.creators[1].backward(Tensor(self.grad.__neg__().data), self)
+
+                if (self.creation_op == "mul"):
+                    new = self.grad * self.creators[1]
+                    self.creators[0].backward(new, self)
+                    new = self.grad * self.creators[0]
+                    self.creators[1].backward(new, self)
+
+                if (self.creation_op == "mm"):
+                    c0 = self.creators[0]
+                    c1 = self.creators[1]
+                    new = self.grad.mm(c1.transpose())
+                    c0.backward(new)
+                    new = self.grad.transpose().mm(c0).transpose()
+                    c1.backward(new)
+
+                if (self.creation_op == "transpose"):
+                    self.creators[0].backward(self.grad.transpose())
+
+                if ("sum" in self.creation_op):
+                    dim = int(self.creation_op.split("_")[1])
+                    self.creators[0].backward(self.grad.expand(dim,
+                                                               self.creators[0].data.shape[dim]))
+
+                if ("expand" in self.creation_op):
+                    dim = int(self.creation_op.split("_")[1])
+                    self.creators[0].backward(self.grad.sum(dim))
+
+                if (self.creation_op == "neg"):
+                    self.creators[0].backward(self.grad.__neg__())
+
     def __add__(self, other):
         if (self.autograd and other.autograd):
             return Tensor(self.data + other.data,
@@ -78,6 +111,68 @@ class Tensor (object):
                         creators=[self, other],
                         creation_op="add")
         return Tensor(self.data + other.data)
+
+    def __neg__(self):
+        if (self.autograd):
+            return Tensor(self.data * -1,
+                          autograd=True,
+                          creators=[self],
+                          creation_op="neg")
+        return Tensor(self.data * -1)
+
+    def __sub__(self, other):
+        if (self.autograd and other.autograd):
+            return Tensor(self.data - other.data,
+                          autograd=True,
+                          creators=[self, other],
+                          creation_op="sub")
+        return Tensor(self.data - other.data)
+
+    def __mul__(self, other):
+        if (self.autograd and other.autograd):
+            return Tensor(self.data * other.data,
+                          autograd=True,
+                          creators=[self, other],
+                          creation_op="mul")
+        return Tensor(self.data * other.data)
+
+    def sum(self, dim):
+        if (self.autograd):
+            return Tensor(self.data.sum(dim),
+                          autograd=True,
+                          creators=[self],
+                          creation_op="sum_" + str(dim))
+        return Tensor(self.data.sum(dim))
+
+    def expand(self, dim, copies):
+
+        trans_cmd = list(range(0, len(self.data.shape)))
+        trans_cmd.insert(dim, len(self.data.shape))
+        new_data = self.data.repeat(copies).reshape(list(self.data.shape) + [copies]).transpose(trans_cmd)
+
+        if (self.autograd):
+            return Tensor(new_data,
+                          autograd=True,
+                          creators=[self],
+                          creation_op="expand_" + str(dim))
+        return Tensor(new_data)
+
+    def transpose(self):
+        if (self.autograd):
+            return Tensor(self.data.transpose(),
+                          autograd=True,
+                          creators=[self],
+                          creation_op="transpose")
+
+        return Tensor(self.data.transpose())
+
+    def mm(self, x):
+        if (self.autograd):
+            return Tensor(self.data.dot(x.data),
+                          autograd=True,
+                          creators=[self, x],
+                          creation_op="mm")
+        return Tensor(self.data.dot(x.data))
 
     def __repr__(self):
         return str(self.data.__repr__())
--- test/main.py
+++ test/main.py
@@ -20,12 +20,78 @@ a = Tensor([1,2,3,4,5], autograd=True)
 b = Tensor([2,2,2,2,2], autograd=True)
 c = Tensor([5,4,3,2,1], autograd=True)
 
-d = a+b
-e = b + c
+d = a+(-b)
+e = (-b) + c
 f = d + e
 
 f.backward(Tensor(np.array([1, 1, 1, 1, 1])))
-print(b.grad.data == np.array([2,2,2,2,2]))
+print(b.grad.data == np.array([-2,-2,-2,-2,-2]))
 
-#Test commit
+x = Tensor(np.array([[1,2,3],
+                     [4,5,6]]))
+
+x.sum(0)
+
+x.sum(1)
+
+print(x.expand(dim=2, copies=4))
+
+print(x)
+#тесты по построению нейронной сети с полученным классом тензор
+#Как было раньше
+
+np.random.seed(0)
+
+data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+target = np.array([[0], [1], [0], [1]])
+
+weights_0_1 = np.random.rand(2, 3)
+weights_1_2 = np.random.rand(3, 1)
+
+for i in range(10):
+    # Predict
+    layer_1 = data.dot(weights_0_1)
+    layer_2 = layer_1.dot(weights_1_2)
+
+    # Compare
+    diff = (layer_2 - target)
+    sqdiff = (diff * diff)
+    loss = sqdiff.sum(0)  # mean squared error loss
+
+    # Learn: this is the backpropagation piece
+    layer_1_grad = diff.dot(weights_1_2.transpose())
+    weight_1_2_update = layer_1.transpose().dot(diff)
+    weight_0_1_update = data.transpose().dot(layer_1_grad)
+
+    weights_1_2 -= weight_1_2_update * 0.1
+    weights_0_1 -= weight_0_1_update * 0.1
+    print(loss[0])
+
+#Как теперь
+np.random.seed(0)
+
+data = Tensor(np.array([[0, 0], [0, 1], [1, 0], [1, 1]]), autograd=True)
+target = Tensor(np.array([[0], [1], [0], [1]]), autograd=True)
+
+w = list()
+w.append(Tensor(np.random.rand(2, 3), autograd=True))
+w.append(Tensor(np.random.rand(3, 1), autograd=True))
+
+for i in range(10):
+
+    # Predict
+    pred = data.mm(w[0]).mm(w[1])
+
+    # Compare
+    loss = ((pred - target) * (pred - target)).sum(0)
+
+    # Learn
+    loss.backward(Tensor(np.ones_like(loss.data)))
+
+    for w_ in w:
+        w_.data -= w_.grad.data * 0.1
+        w_.grad.data *= 0
+
+    print(loss)
+
 g=0
