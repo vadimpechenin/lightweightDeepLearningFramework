cd6683cc66d5e83cb8376a6dc3af4c9d7e7da1a5
My Name <vadim.pechenin2015@gmail.com>
2021-12-05 10:53:12 +0300

    Add SGD

--- test/main.py
+++ test/main.py
@@ -68,7 +68,7 @@ for i in range(10):
     print(loss[0])
 
 #Как теперь
-np.random.seed(0)
+#np.random.seed(0)
 
 data = Tensor(np.array([[0, 0], [0, 1], [1, 0], [1, 1]]), autograd=True)
 target = Tensor(np.array([[0], [1], [0], [1]]), autograd=True)
@@ -94,4 +94,35 @@ for i in range(10):
 
     print(loss)
 
+#Как теперь с оптимизатором градиентного спуска
+from optimization.sgd import SGD
+#np.random.seed(0)
+
+data = Tensor(np.array([[0, 0], [0, 1], [1, 0], [1, 1]]), autograd=True)
+target = Tensor(np.array([[0], [1], [0], [1]]), autograd=True)
+
+w = list()
+w.append(Tensor(np.random.rand(2, 3), autograd=True))
+w.append(Tensor(np.random.rand(3, 1), autograd=True))
+
+optim = SGD(parameters=w, alpha=0.1)
+
+for i in range(10):
+
+    # Predict
+    pred = data.mm(w[0]).mm(w[1])
+
+    # Compare
+    loss = ((pred - target) * (pred - target)).sum(0)
+
+    # Learn
+    loss.backward(Tensor(np.ones_like(loss.data)))
+
+    for w_ in w:
+        w_.data -= w_.grad.data * 0.1
+        w_.grad.data *= 0
+
+    print(loss)
+
 g=0
+
